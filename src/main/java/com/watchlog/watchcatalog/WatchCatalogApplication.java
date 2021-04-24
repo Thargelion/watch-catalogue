7eda8cda27b0d8973b489ce330ee9ff33b725d15
Maximiliano De Pietro <elwindwalker@gmail.com>
2021-04-24 17:08:33 -0300

    Add Spring Project :hammer:

---
+++ src/main/java/com/watchlog/watchcatalog/WatchCatalogApplication.java
@@ -0,0 +1,13 @@
+package com.watchlog.watchcatalog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WatchCatalogApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WatchCatalogApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/watchlog/watchcatalog/WatchCatalogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.watchlog.watchcatalog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WatchCatalogApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
