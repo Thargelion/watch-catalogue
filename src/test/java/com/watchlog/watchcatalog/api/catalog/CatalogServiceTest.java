eb31dfccaddb9532b4e3ee7a15455b9c5f85389e
Maximiliano De Pietro <elwindwalker@gmail.com>
2021-04-25 22:28:13 -0300

    :white_check_mark: test with mocked dependencies'

---
+++ src/test/java/com/watchlog/watchcatalog/api/catalog/CatalogServiceTest.java
@@ -0,0 +1,29 @@
+package com.watchlog.watchcatalog.api.catalog;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.MockitoAnnotations;
+
+public class CatalogServiceTest {
+
+    @InjectMocks
+    CatalogService subject;
+
+    @BeforeEach
+    void setUp() {
+        MockitoAnnotations.openMocks(this);
+    }
+
+    @Test
+    void getTotalPrice_getsTotalPrice() {
+        // Given
+        var expectedPrice = 20L;
+        var input = new String[]{"10"};
+        // When
+        var actualTotalPrice = subject.getTotalPrice(input);
+        // Then
+        Assertions.assertEquals(expectedPrice, actualTotalPrice.getPrice());
+    }
+}
