c5da1daf65f5c531cd9343d327c37b5b2d56362c
Maximiliano De Pietro <elwindwalker@gmail.com>
2021-04-25 22:10:15 -0300

    :hammer: add integration test

---
+++ src/main/java/com/watchlog/watchcatalog/config/SpringConfig.java
@@ -0,0 +1,10 @@
+package com.watchlog.watchcatalog.config;
+
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@EnableAutoConfiguration
+public class SpringConfig implements WebMvcConfigurer {
+}
